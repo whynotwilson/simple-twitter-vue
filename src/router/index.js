4b46bacf169bd1c7cee5d59d5060e56ebfa9749b
Wilson Tseng <p780729@hotmail.com>
2021-05-27 16:28:57 +0800

    Add page - SignIn

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import PageNotFound from '../views/PageNotFound.vue'
+import SignIn from '../views/SignIn.vue'
 
 const routes = [
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
   {
     path: '/:pathMatch(.*)*',
     name: 'not-found',
---
+++ src/views/SignIn.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="form-signin mt-5">
+    <form class="w-100" @submit.prevent="handleSubmit">
+      <div class="text-center mb-4">
+        <h1 class="h2 mb-3 font-weight-normal">Sign In</h1>
+      </div>
+
+      <div class="form-label-group mb-3">
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="email"
+          autocomplete="username"
+          required
+          autofocus
+        />
+        <input
+          id="password"
+          v-model="password"
+          name="password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="current-password"
+          required
+        />
+      </div>
+
+      <div class="text-center">
+        <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">
+          Submit
+        </button>
+      </div>
+
+      <div class="text-center mb-3">
+        <p>
+          <router-link to="/signup"> Sign Up </router-link>
+        </p>
+      </div>
+
+      <p class="mt-5 mb-3 text-muted text-center">&copy; 2021</p>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    return {
+      email: ref(""),
+      password: ref(""),
+    };
+  },
+
+  methods: {
+    handleSubmit(e) {
+      const data = JSON.stringify({
+        email: this.email,
+        password: this.password,
+      });
+
+      // TODO: 向後端驗證使用者登入資訊是否合法
+      console.log(e);
+      console.log("data", data);
+    },
+  },
+};
+</script>
+
+<style>
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+  background-color: #ffffff;
+  border-radius: 12px;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+</style>
