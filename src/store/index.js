7a366d3161d7149274342dc3a6ed694b631b609a
Wilson Tseng <p780729@hotmail.com>
2021-09-01 13:46:16 +0800

    fix: integrate API of getCurrentUser in store actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import usersAPI from './../apis/users.js'
 
 export default createStore({
   // 類似 data，但在所有 component 都能讀取
@@ -25,6 +26,26 @@ export default createStore({
 
   // 透過 API 請求資料
   actions: {
+    async fetchCurrentUser({ commit }) {
+      try {
+        const { data } = await usersAPI.getCurrentUser()
+        if (data.status === "error") {
+          throw new Error(data.message)
+        }
+
+        const { id, name, email, avatar } = data
+
+        commit('setCurrentUser', {
+          id,
+          name,
+          email,
+          avatar,
+        })
+
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    }
   },
 
   modules: {
