bd0d03b9e22f4f8af8e0a587ebc618ab2537589d
Wilson Tseng <p780729@hotmail.com>
2021-09-01 10:41:59 +0800

    Setup state and mutations in Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,32 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // 類似 data，但在所有 component 都能讀取
   state: {
+    currentUser: {
+      id: -1,
+      name: "",
+      email: "",
+      avatar: "",
+    },
+    isAuthenticated: false,
   },
+
+  // 用來修改 state 的方法
   mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        ...currentUser,
+      }
+      state.isAuthenticated = true
+    },
   },
+
+  // 透過 API 請求資料
   actions: {
   },
+
   modules: {
   }
 })
