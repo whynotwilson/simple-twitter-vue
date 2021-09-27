4ed08f426bf674da4f41e52f31696aecc9808c64
Wilson Tseng <p780729@hotmail.com>
2021-09-27 16:57:38 +0800

    Add hashtag-textarea and remove textarea of CreateTweet component

--- src/components/CreateTweet.vue
+++ src/components/CreateTweet.vue
@@ -8,24 +8,38 @@
     @submit.prevent="handleSubmit"
   >
     <div class="text-align-end">
-      <div class="d-flex mt-4 mb-3 mx-4">
+      <div class="d-flex mt-4 mb-2 mx-4">
         <router-link
           class="rounded-pill"
           :to="{ name: 'user', params: { id: currentUser.id } }"
         >
           <img class="avatar" :src="currentUser.avatar" alt="avatar" />
         </router-link>
-
-        <textarea
-          :placeholder="currentUser.name + '，在想些什麼呢？'"
-          class="form-control ms-2"
-          v-model="tweetText"
-          cols="20"
-          rows="5"
-        ></textarea>
+        <div class="ms-2">
+          {{ currentUser.name }}
+        </div>
       </div>
 
-      <div class="mb-3 mx-4">
+      <div class="mx-4 mb-2">
+        <div class="hashtag-textarea">
+          <div
+            id="textarea-overlay"
+            class="hashtag-textarea__overlay"
+            v-html="tweetText"
+          ></div>
+          <div
+            id="inputTrueText"
+            @input="replaceTweetText($event.target.innerText)"
+            class="hashtag-textarea__true-text"
+            wrap="soft"
+            contentEditable="true"
+            :placeholder="currentUser.name + '，在想些什麼呢？'"
+            spellcheck="false"
+          ></div>
+        </div>
+      </div>
+
+      <div class="mb-2 mx-4">
         <button
           type="submit"
           class="btn btn-primary"
@@ -37,6 +51,52 @@
     </div>
   </form>
 </template>
+
+<style>
+.hashtag-textarea {
+  position: relative;
+  overflow: auto;
+  border: 1px solid #e4e4e4;
+  border-radius: 5px;
+  background: white;
+  min-height: 180px;
+  height: 100%;
+  width: 100%;
+}
+.hashtag-textarea__true-text {
+  padding: 0;
+  margin: auto 0px;
+  height: 100%;
+  width: 100%;
+  white-space: pre-wrap;
+  overflow-wrap: break-word;
+  border: 0;
+  background: transparent;
+  position: absolute;
+  top: 0;
+  color: transparent;
+  caret-color: black;
+  outline: none;
+  resize: none;
+  text-align: left;
+}
+.hashtag-textarea__overlay {
+  padding: 0;
+  height: 100%;
+  min-height: 178px;
+  width: 100%;
+  white-space: pre-wrap;
+  overflow-wrap: break-word;
+  border: 0;
+  caret-color: black;
+  outline: none;
+  resize: none;
+  text-align: left;
+}
+.hashtag {
+  background: #d1e4fc;
+}
+</style>
 
 <script>
 import { ref, computed } from "vue";
@@ -64,6 +124,29 @@ export default {
     let isProcessing = ref(false);
     let tweetText = ref("");
 
+    let reg = new RegExp(
+      // /(^|\B)#([a-zA-Z0-9_\u4e00-\u9fa5]+)/
+      /(^|\B)#(?![0-9_]+\b)([a-zA-Z0-9_\u4e00-\u9fa5]+)/,
+      "g"
+    );
+
+    let hashtags = computed(() => {
+      if (reg.test(tweetText.value)) {
+        return tweetText.value.match(reg);
+      } else {
+        return [];
+      }
+    });
+
+    const replaceTweetText = (newValue) => {
+      newValue = newValue.replace(reg, function(match) {
+        return `<span><a class="hashtag" href="/#/hashtag/${match
+          .toLowerCase()
+          .replace("#", "")}">${match}</a></span>`;
+      });
+      tweetText.value = newValue;
+    };
+
     const isTweetTextEmpty = computed(() => {
       return tweetText.value ? false : true;
     });
@@ -81,23 +164,11 @@ export default {
         isProcessing.value = true;
         tweetText.value = tweetText.value.replace(/\n/g, "<br/>");
 
-        let reg = new RegExp(/\B#[0-9_a-zA-Z\u4e00-\u9fa5]+/, "g");
-        let hasOneWord = new RegExp(/[a-zA-Z\u4e00-\u9fa5]+/, "g");
-
-        let tags = [];
-        let tag;
-        while ((tag = reg.exec(tweetText.value)) !== null) {
-          // tag can not be only number or only underline
-          // expect #12345 false
-          // expect #_____ false
-          if (tag[0].match(hasOneWord)) {
-            tags.push(tag[0].substr(1).toLowerCase());
-          }
-        }
-
         const { data } = await tweetsAPI.create({
           tweetText: tweetText.value,
-          tags,
+          tags: hashtags.value.map((hashtag) => {
+            return hashtag.toLowerCase().replace("#", "");
+          }),
         });
 
         if (data.status !== "success") {
@@ -110,6 +181,7 @@ export default {
 
         emit("after-create-tweet", tweet);
         tweetText.value = "";
+        document.getElementById("inputTrueText").innerText = "";
       } catch (error) {
         Toast.fire({
           icon: "error",
@@ -123,6 +195,8 @@ export default {
     };
 
     return {
+      hashtags,
+      replaceTweetText,
       isMyPage,
       isProcessing,
       tweetText,
